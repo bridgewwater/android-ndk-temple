3147904a4b253f7dbef14c47053688e083c627b5
sinlov <sinlovgmppt@gmail.com>
2023-06-06 00:31:50 +0800

    build: fix z-ndkmodule.mk

--- ndkmodule/z-ndkmodule.mk
+++ ndkmodule/z-ndkmodule.mk
@@ -26,10 +26,10 @@ ndkmoduleTestRelease: ndkmoduleClean
 	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):testReleaseUnitTest
 
 ndkmoduleJacocoReportDebug:
-	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_plugin):jacocoDebugReport
+	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):jacocoDebugReport
 
 ndkmoduleJacocoReportRelease:
-	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_plugin):jacocoReleaseReport
+	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):jacocoReleaseReport
 
 ndkmoduleRelease:
 	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):build
@@ -41,30 +41,30 @@ ndkmoduleAssembleRelease: ndkmoduleClean
 	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):assembleRelease
 
 ndkmodulePublishToMavenLocal:
-	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_plugin):publishToMavenLocal
+	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):publishToMavenLocal
 	$(info will publish at $$HOME/.m2/repository)
 
 ndkmodulePublish:
-	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_plugin):publish
+	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):publish
 
 ndkmoduleUploadArchives: ndkmoduleClean
 	$(ROOT_PWD)/gradlew -q $(ROOT_MODULE_ndkmodule):uploadArchives
 
 help-ndkmodule:
 	@echo "=> ${ROOT_PWD}/${ROOT_MODULE_ndkmodule}/z-ndkmodule.mk : android library module [ $(ROOT_MODULE_ndkmodule) ] task"
-	@echo "make $(ROOT_MODULE_plugin)Tasks                         ~> show task of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)Clean                         ~> clean of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)DependImplementation          ~> see dependencies implementation of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)DependReleaseCompileClasspath ~> see dependencies implementation of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)Test                          ~> run test of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)TestDebug                     ~> run test Debug of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)TestRelease                   ~> run test Release of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)JacocoReportDebug             ~> run report jacoco debug of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)JacocoReportRelease           ~> run report jacoco release of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)Release                       ~> build release of module [ $(ROOT_MODULE_plugin) ]"
-	@echo "make $(ROOT_MODULE_plugin)AssembleDebug                 ~> assemble debug of module [ :$(ROOT_MODULE_plugin):assembleDebug ]"
-	@echo "make $(ROOT_MODULE_plugin)AssembleRelease               ~> assemble release of module [ :$(ROOT_MODULE_plugin):assembleRelease ]"
-	@echo "make $(ROOT_MODULE_plugin)UploadArchives                ~> module [ :$(ROOT_MODULE_plugin):uploadArchives ] deprecated by at gradle 7.+"
-	@echo "make $(ROOT_MODULE_plugin)Publish                       ~> module [ :$(ROOT_MODULE_plugin):publish ] effect at gradle 7.+"
-	@echo "make $(ROOT_MODULE_plugin)pluginPublishToMavenLocal     ~> module [ :$(ROOT_MODULE_plugin):publishToMavenLocal ] effect at gradle 7.+"
+	@echo "make $(ROOT_MODULE_ndkmodule)Tasks                         ~> show task of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)Clean                         ~> clean of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)DependImplementation          ~> see dependencies implementation of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)DependReleaseCompileClasspath ~> see dependencies implementation of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)Test                          ~> run test of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)TestDebug                     ~> run test Debug of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)TestRelease                   ~> run test Release of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)JacocoReportDebug             ~> run report jacoco debug of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)JacocoReportRelease           ~> run report jacoco release of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)Release                       ~> build release of module [ $(ROOT_MODULE_ndkmodule) ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)AssembleDebug                 ~> assemble debug of module [ :$(ROOT_MODULE_ndkmodule):assembleDebug ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)AssembleRelease               ~> assemble release of module [ :$(ROOT_MODULE_ndkmodule):assembleRelease ]"
+	@echo "make $(ROOT_MODULE_ndkmodule)UploadArchives                ~> module [ :$(ROOT_MODULE_ndkmodule):uploadArchives ] deprecated by at gradle 7.+"
+	@echo "make $(ROOT_MODULE_ndkmodule)Publish                       ~> module [ :$(ROOT_MODULE_ndkmodule):publish ] effect at gradle 7.+"
+	@echo "make $(ROOT_MODULE_ndkmodule)pluginPublishToMavenLocal     ~> module [ :$(ROOT_MODULE_ndkmodule):publishToMavenLocal ] effect at gradle 7.+"
 	@echo ""
